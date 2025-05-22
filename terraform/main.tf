aa207cbdca0453d2f297f756fd11e60db255cfbf
SwathaManoharan <swethamanoharan2001@gmail.com>
2025-05-22 22:01:13 +0530

    ../added

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ resource "aws_iam_role_policy_attachment" "lambda_attach" {
 
 
 resource "aws_lambda_layer_version" "layer" {
-  filename            = "layer.zip"
+  filename            = "../layer.zip"
   layer_name          = "${var.layer_name}-layer"
   compatible_runtimes = [var.runtime]
   source_code_hash    = filebase64sha256("layer.zip")
