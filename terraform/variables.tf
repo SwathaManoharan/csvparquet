a0d3f75e2c5a9fa6692868cd3d7d061393f01db1
SwathaManoharan <swethamanoharan2001@gmail.com>
2025-05-22 16:22:19 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,67 +1,10 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-
-
-variable "source_bucket_name" {
-  description = "Source S3 bucket name"
-  type        = string
-}
-
-variable "destination_bucket_name" {
-  description = "Destination S3 bucket name"
-  type        = string
-}
-
-variable "notification_email" {
-  description = "SES verified email for notifications"
-  type        = string
-}
-
-variable "lambda_function_name" {
-  description = "Lambda function name"
-  type        = string
-}
-
-variable "lambda_handler" {
-  description = "Lambda handler (file.function)"
-  type        = string
-  default     = "lambda_function.lambda_handler"
-}
-
-variable "runtime" {
-  description = "Lambda runtime"
-  type        = string
-  default     = "python3.9"
-}
-
-variable "layer_name" {
-  description = "Lambda layer name"
-  type        = string
-}
-
-variable "layer_s3_key" {
-  description = "S3 key for the Lambda layer zip"
-  type        = string
-  default     = "layer.zip"
-}
-
-variable "code_s3_key" {
-  description = "S3 key for the Lambda function zip"
-  type        = string
-  default     = "lambda.zip"
-}
-variable "artifact_bucket_name" {
-  description = "S3 bucket name for storing Lambda artifacts"
-  type        = string
-}
-
-variable "ecr_image_uri" {
-  description = "ECR image URI for Lambda container"
-  default = "402181693603.dkr.ecr.ap-south-1.amazonaws.com/csv-to-parquet"
-}
-
-
+variable "aws_region" {}
+variable "artifact_bucket_name" {}
+variable "source_bucket_name" {}
+variable "destination_bucket_name" {}
+variable "notification_email" {}
+variable "lambda_function_name" {}
+variable "runtime" {}
+variable "lambda_s3_key" {}
+variable "layer_s3_key" {}
+variable "layer_name" {}
